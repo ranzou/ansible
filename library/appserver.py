9f329ed827ee11988961066785ef27d5fc788b16
Shaun George <Shaun.GEORGE@IMMI.gov.au>
2017-07-28 12:56:33 +1000

    Remove dependancy on passed in profileName parameter
    It will now work out what profiles are present on the host and stop all their associated appservers

--- library/appserver.py
+++ library/appserver.py
@@ -2,7 +2,8 @@
 
 #
 # Stop all application servers
-#
+# Author - Shaun George
+# Version 0.1 - Initial
 
 import os
 from subprocess import Popen, PIPE
@@ -16,12 +17,12 @@ def main():
     module = AnsibleModule(
         argument_spec = dict(
             state   = dict(default='status', choices=['stopped', 'started', 'status']),
-            profileName = dict(required=True)
+            psall   = dict(required=True)
         )
     )
 
     state = module.params['state']
-    profileName = module.params['profileName']
+    psall = module.params['psall']
     wasdir = '/apps/websphere/appserver/8.5'
 
     # Check if paths are valid
@@ -30,7 +31,7 @@ def main():
 
     # First, grab a string of all the application servers on the host
     # child = Popen([wasdir+"/bin/serverStatus.sh -all -profileName " + profileName], shell=True, stdout=PIPE, stderr=PIPE)
-    child = Popen(["/apps/ebus/build/etc/linux/scripts/psall.sh"], stdout=PIPE, stderr=PIPE)
+    child = Popen([psall], stdout=PIPE, stderr=PIPE)
     stdout_value, stderr_value = child.communicate()
     if state == 'status':
         module.exit_json(changed=False, msg=stdout_value, stdout=stdout_value)
@@ -40,27 +41,38 @@ def main():
         else:
             command = 'start'
         serverList = []
+        # Iterate over instances like wasserver:BSN85Cell01Node01:ECS01
         for m in re.finditer('wasserver:\w+:\w+', stdout_value):
             # m.group(0) is the actual text rather than the match object
             server = m.group(0)
+            # Isolate the ECS01 part of the string in, for example, wasserver:BSN85Cell01Node01:ECS01 
             server = server.split(':')[2]
             serverList.append(server)
 
-        # Stop or start all the servers in serverList concurrently, storing their child processes in the 'processes' list for later access
-        processes = []
-        for server in serverList:
-            child = Popen([wasdir + "/profiles/" + profileName + "/bin/" + command + "Server.sh", server], stdout=PIPE, stderr=PIPE)
-            # Add server name as well as child process, so I can access it in the loop below
-            processes.append([server,child])
+        # Gather list of nodes
+        nodeList = []
+        for m in re.finditer('node:\w+Node01', stdout_value):
+            node = m.group(0)
+            node = node.split(':')[1]
+            nodeList.append(node)
+
+        for node in nodeList:
+            # Stop or start all the servers in serverList concurrently, storing their child processes in the 'processes' list for later access
+            processes = []
+            for server in serverList:
+                child = Popen([wasdir + "/profiles/" + node + "/bin/" + command + "Server.sh", server], stdout=PIPE, stderr=PIPE)
+                # Add server name as well as child process, so I can access it in the loop below
+                processes.append([node,server,child])
 
         # Retrieve the results of all the child processes and add output to list 'messages', which will be returned to the user
         messages = []
         for process in processes:
-            server = process[0]
-            child = process[1]
+            node = process[0]
+            server = process[1]
+            child = process[2]
             stdout_value, stderr_value = child.communicate()
             if child.returncode != 0:
-                messages.append("%s %s %s failed" % (profileName, server, command))
+                messages.append("%s %s %s failed" % (node, server, command))
 
         if not messages:
             output = "All servers %s successfully" % state
